adc2137dc6dbcec8ceda21af67703e7a24cf53cb
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-02-27 10:08:11 -0300

    Mudar o header de acordo com o click

--- src/app/modules/portifolio/components/header/header.component.ts
+++ src/app/modules/portifolio/components/header/header.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent  {
-  
+export class HeaderComponent {
+  constructor() {}
+  currentPage: string | number = 'home'
+
+
+
+
+
+  //Header click action -------------------------------------------------
+  updateCurrentPage(page: string | number){
+    this.currentPage = page
+  }
+
+  scrollElement(elementId: string | number): void {
+    if (typeof elementId === 'string') {
+      const element = document.getElementById(elementId);
+      const active = document.getElementsByClassName(elementId);
+      if (element) {
+        this.updateCurrentPage(elementId)
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }else if (typeof elementId === 'number'){
+      this.updateCurrentPage(elementId)
+      window.scrollTo({top: elementId, behavior: 'smooth'})
+    }
+  }
 }
