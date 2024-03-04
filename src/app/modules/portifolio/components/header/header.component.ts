defaf286083f895bec8d21d59b093c9adee10dfa
GMPompeu <gui.pompeu.mascarenhas@gmail.com>
2024-03-04 14:48:50 -0300

    Responsive start

--- src/app/modules/portifolio/components/header/header.component.ts
+++ src/app/modules/portifolio/components/header/header.component.ts
@@ -25,7 +25,7 @@ export class HeaderComponent {
       const sectionId = sections.getAttribute('id');
       const sectionOffset = sections.getBoundingClientRect().top;
 
-      if (sectionOffset <= 300 && sectionId !== null) {
+      if (sectionOffset <= 500 && sectionId !== null) {
         this.updateCurrentPage(sectionId);
       }
     });
